1637e0862dfc0f6d1608b7f75b9cd97251f8bc01
jonathan <jonathan@debian.localdomain>
2011-12-24 21:30:58 +0100

    Add table definition

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ In your migrations:
 	class CreateUsers < ActiveRecord::Migration
 		def self.up
 			create_table :users do |t|
-				t.string :uid, :null => false
+				t.uid
 			end
 			add_index :users, :uid, :uniq => true
 		end
--- lib/acts_as_uid.rb
+++ lib/acts_as_uid.rb
@@ -59,3 +59,15 @@ module ActiveRecord::Acts::ActsAsUid
 end
 
 ActiveRecord::Base.send(:include, ActiveRecord::Acts::ActsAsUid)
+
+
+module ActiveRecord; module ConnectionAdapters; end; end
+module ActiveRecord::ConnectionAdapters::ActsAsUidTableDefinition
+	
+	def uid
+		column :uid, :string, :null => false
+	end
+	
+end
+
+ActiveRecord::ConnectionAdapters::TableDefinition.send(:include, ActiveRecord::ConnectionAdapters::ActsAsUidTableDefinition)
